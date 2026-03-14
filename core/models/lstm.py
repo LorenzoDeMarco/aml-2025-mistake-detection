7707362fecfd9ea02f1de55bba9c9edc383d0d34
Lorenzo De Marco <lorenzo.demarco002@gmail.com>
2026-03-14 19:57:28 +0100

    update lstm

--- core/models/lstm.py
+++ core/models/lstm.py
@@ -1,38 +1,46 @@
 import torch
 from torch import nn
-from core.models.blocks import MLP, fetch_input_dim
+from core.models.blocks import fetch_input_dim
 
 class LSTMModel(nn.Module):
-
     def __init__(self, config):
         super().__init__()
-        self.config= config
-        input_dimension = fetch_input_dim(config)
-
-        self.hidden_dim = getattr(config, 'lstm_hidden_dim', 256)
-        self.num_layers = getattr(config, 'lstm_layers', 2)
-
-        # Layer LSTM
-        self.lstm= nn.LSTM(
-            input_size=input_dimension,
+        self.config = config
+        input_dim = fetch_input_dim(config)
+        
+        self.hidden_dim = 256
+        self.num_layers = 2
+        
+        # Bi-LSTM
+        self.lstm = nn.LSTM(
+            input_size=input_dim,
             hidden_size=self.hidden_dim,
             num_layers=self.num_layers,
             batch_first=True,
             bidirectional=True,
-            dropout=0.3 if self.num_layers > 1 else 0.0
+            dropout=0.3 if self.num_layers > 1 else 0
+        )
+        
+
+        self.classifier = nn.Sequential(
+            nn.Linear(self.hidden_dim * 2, 256),
+            nn.ReLU(),
+            nn.Dropout(0.3),
+            nn.Linear(256, 1)
         )
 
-        # Decoder MLP
-        self.decoder = MLP(self.hidden_dim * 2, 512, 1)  # *2 for bidirectional
-
     def forward(self, x):
-        # x shape: (batch, seq_len, feature_dim)
+        # x shape: [Batch, Time, Features] 
         x = torch.nan_to_num(x, nan=0.0)
-
-        # lsto_out shape: (batch, seq_len, hidden_dim * 2)
-        lstm_out, (h_n, c_n) = self.lstm(x)
-
-        # Global Average Pooling over the sequence dimension
-        encoded_output = torch.mean(lstm_out, dim=1)
-
-        return self.decoder(encoded_output)
+        
+        # lstm_out: [Batch, Time, Hidden*2]
+        # h_n: [Layers*2, Batch, Hidden]
+        _, (h_n, _) = self.lstm(x)
+        
+        h_fwd = h_n[-2, :, :] # Shape: [Batch, Hidden]
+        h_bwd = h_n[-1, :, :] # Shape: [Batch, Hidden]
+        
+        # [Batch, Hidden*2] -> [1, 512]
+        combined_state = torch.cat([h_fwd, h_bwd], dim=1)
+        
+        return self.classifier(combined_state)
