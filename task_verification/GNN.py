4161bc66352c0d948ffb11ab3d636c23ebcfeb90
Lorenzo De Marco <lorenzo.demarco002@gmail.com>
2026-05-18 19:07:45 +0200

    fix

--- task_verification/GNN.py
+++ task_verification/GNN.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from torch_geometric.nn import GraphConv, global_mean_pool, global_max_pool
 
-from task_verification.matching import GraphNodeRealizer
+from matching import GraphNodeRealizer
 
 
 class TaskVerificationGNN(nn.Module):
